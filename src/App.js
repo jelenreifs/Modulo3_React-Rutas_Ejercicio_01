39229c053d898fce3ab37baf6652aff6a696dfdb
REIFS HERNANDO <ereifs@serikat.es>
2020-12-02 08:20:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+//import {BrowserRouter, Route, Link} from 'react-router-dom'
+import { useState } from "react";
+
+
+const arrayNombres = [
+    'Wolfgang Amadeus Mozart',
+    'Ludwig van Beethoven',
+    'Johann Sebastian Bach'
+];
+
+
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+  const [text, setText] = useState(inputValue);
+  const [list, setList] = useState(arrayNombres);
+  const [name, setName] = useState(arrayNombres)
+ 
+
+
+ // Capturar el valor del input a través del evento onChange
+
+  const handleInput = (e) => { 
+    setInputValue(e.target.value)
+  }
+  const showText = () => {
+    setText(inputValue)
+  }
+
+  const nombres = arrayNombres.map(nombre => {
+    return <li key={ nombre }>{nombre}</li>
+  })
+
+
+
+  const showList = () => setList(nombres);
+
+    const handleInput2 = (e) => { 
+    setInputValue2(e.target.value)
+  }
+
+    const mostrarNombres = name.map(name => {
+    return <li key={name}>{name}</li>
+  })
+
+  const addName = () => {
+    setName((prevArray) => {return[...prevArray, handleInput2]})
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <input type="text"
+      value={inputValue}
+      onChange={handleInput} />
+      
+      <button onClick={showText}>Mostrar texto</button>
+      <p>{text}</p>
+
+       <hr />
+
+      <button onClick={showList}>Mostrar Lista</button>
+      <ul>{list}</ul>
+
+      <hr /> 
+
+      <input type="text"
+      value={inputValue2}
+      onChange={handleInput2} />
+
+      <button onClick={addName}>Añadir nombre</button>
+      <ul>{mostrarNombres}</ul>
+        
+       </>
   );
 }
 
 export default App;
+
